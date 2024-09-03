1a35ee3c0a509d70b424d292133c5de1dab77826
shzahab <zahab@hey.com>
2024-09-04 02:19:10 +0530

    Upload existing project files to Git

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
   },
-}
+  // Add any other configurations here
+};
 
 module.exports = nextConfig
